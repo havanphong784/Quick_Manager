b8ec9c0344e6112c2bca3a931ab28ae9754159db
Phongha <phongha@gmail.com>
2026-04-01 20:33:18 +0700

    add event load info SP

--- src/main/java/com/quickmanager/controller/ProductController.java
+++ src/main/java/com/quickmanager/controller/ProductController.java
@@ -5,7 +5,11 @@ import com.quickmanager.service.ProductService;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,12 +32,26 @@ public class ProductController {
     @FXML private Label lblTongSoSP;
 
 
+    //TTSP
+    @FXML private TextField txtFormMaSP;
+    @FXML private TextField txtFormTenSP;
+    @FXML private TextField txtFormDanhMuc;
+    @FXML private TextField txtFormDVT;
+    @FXML private TextField txtFormTonKho;
+    @FXML private TextField txtFormGiaNhap;
+    @FXML private TextField txtFormGiaBan;
+    @FXML private TextField txtFormTrangThai;
+
+
     public void initialize() {
         initDSSP();
         handleTimKiem();
         loadDSSP();
         loadDanhMuc();
         loadTrangThai();
+        tbSanPham.getSelectionModel().selectedItemProperty().addListener((obs, spc, spm) -> {
+            handleSelectTB();
+        });
     }
 
     // DSSP
@@ -69,4 +87,31 @@ public class ProductController {
         loadDSSP();
         lblTongSoSP.setText("Tổng số sản phẩm: " + mangSanPham.size());
     }
+
+    public void handleSelectTB() {
+        SanPham sp = tbSanPham.getSelectionModel().getSelectedItem();
+        if (sp != null) {
+            txtFormMaSP.setText(String.valueOf(sp.getMaSanPham()));
+            txtFormTenSP.setText(sp.getTenSanPham());
+            txtFormDanhMuc.setText(sp.getTenDanhMuc());
+            txtFormDVT.setText(sp.getDonViTinh());
+            txtFormTonKho.setText(String.valueOf(sp.getSoLuongTon()));
+            txtFormGiaNhap.setText(String.valueOf(sp.getGiaNhap()));
+            txtFormGiaBan.setText(String.valueOf(sp.getGiaBan()));
+            txtFormTrangThai.setText(sp.getTrangThai());
+            txtFormDanhMuc.setText(sp.getTenDanhMuc());
+        }else {
+            txtFormDanhMuc.clear();
+            txtFormDVT.clear();
+            txtFormGiaBan.clear();
+            txtFormGiaNhap.clear();
+            txtFormMaSP.clear();
+            txtFormTenSP.clear();
+            txtFormTrangThai.clear();
+            txtFormTonKho.clear();
+            txtFormGiaNhap.clear();
+            txtFormGiaBan.clear();
+        }
+    }
+
 }
