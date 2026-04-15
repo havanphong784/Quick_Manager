ddad6c45ac1f8072844b8ed413edd54e967d3ebb
Phongha <havanphong784@gmail.com>
2026-04-15 08:28:09 +0700

    Delegate permissions

--- src/main/java/com/quickmanager/controller/DashBoardController.java
+++ src/main/java/com/quickmanager/controller/DashBoardController.java
@@ -7,9 +7,14 @@ import com.quickmanager.service.SessionService;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -17,14 +22,19 @@ import java.util.Objects;
 import static com.quickmanager.Main.isDarkTheme;
 
 public class DashBoardController {
-    @FXML
-    private AnchorPane contentPane;
-    
-    @FXML
-    private Label accountLabel;
-    
-    @FXML
-    private Label roleLabel;
+    @FXML private AnchorPane contentPane;
+    @FXML private Label accountLabel;
+    @FXML private Label roleLabel;
+    @FXML VBox menu;
+    @FXML private ToggleButton btnMenuHome;
+    @FXML private ToggleButton btnMenuSell;
+    @FXML private ToggleButton btnMenuImport;
+    @FXML private ToggleButton btnMenuInvoice;
+    @FXML private ToggleButton btnMenuProduct;
+    @FXML private ToggleButton btnMenuEmployee;
+    @FXML private ToggleButton btnMenuStatistics;
+    @FXML private HBox menuFooter;
+    @FXML private Region menuRegion;
 
     private double dragOffsetX;
     private double dragOffsetY;
@@ -32,6 +42,7 @@ public class DashBoardController {
     @FXML
     public void initialize() {
         TaiKhoan tk = SessionService.getUser();
+        handleRole(tk);
         accountLabel.setText(tk.getTenDangNhap());
         roleLabel.setText(tk.getVaiTro());
         setPage("home.fxml");
@@ -67,6 +78,25 @@ public class DashBoardController {
         isDarkTheme = !isDarkTheme;
         Main.stage.getScene().getStylesheets().clear();
         Main.stage.getScene().getStylesheets().add(Objects.requireNonNull(getClass().getResource(isDarkTheme ? "/view/index.css" : "/view/light.css")).toExternalForm());
+    }
+
+    public void handleRole(TaiKhoan tk) {
+        menu.getChildren().clear();
+        if (tk.getVaiTro().equals("ADMIN")) {
+            menu.getChildren().add(btnMenuHome);
+            menu.getChildren().add(btnMenuSell);
+            menu.getChildren().add(btnMenuImport);
+            menu.getChildren().add(btnMenuInvoice);
+            menu.getChildren().add(btnMenuProduct);
+            menu.getChildren().add(btnMenuEmployee);
+            menu.getChildren().add(btnMenuStatistics);
+        }else {
+            menu.getChildren().add(btnMenuHome);
+            menu.getChildren().add(btnMenuSell);
+            menu.getChildren().add(btnMenuImport);
+        }menu.getChildren().add(menuRegion);
+        menu.getChildren().add(menuFooter);
+
     }
 
     @FXML
